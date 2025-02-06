63ca260d838440ace2642574540b4be66f655d28
Rahim <ethanrahimmichuki.com>
2025-02-06 17:51:27 +0300

    added key : value

--- index.js
+++ index.js
@@ -11,16 +11,25 @@ let details = {
 }
 
 const firstName = document.getElementById("firstName")
+const lastName = document.getElementById("lastName")
 const submitBtn = document.getElementById("submitBtn")
 
 firstName.addEventListener("change", (e) => {
     const name = e.target.value
     details.firstName = name
+    console.log(e.target.value)
+})
+
+lastName.addEventListener("change", (e) => {
+    const name = e.target.value
+    details.lastName = name 
+    console.log(e.target.value)
 })
 
 phoneNumber.addEventListener("change", (e) => {
     const phoneNumber = e.target.value
     details.phoneNumber = phoneNumber
+    console.log(e.target.value)
     // Using regular expression, validate if the number is a valid kenyan number
     // if it's not, using the alert function, notify the user that the
 //number is not valid
@@ -38,4 +47,5 @@ submitBtn.addEventListener("click", () => {
 confirmPassword, agreeTerms} = details
 
     validatePassword(password, confirmPassword)
+    
 })
